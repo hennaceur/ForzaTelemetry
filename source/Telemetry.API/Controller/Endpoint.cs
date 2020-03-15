915de61c9fa1f1d062c5a342bae734b6d00a8bf6
Hazem <hazem@inspectar.com>
2020-03-14 22:44:33 -0230

    Set up MongoDB for connection

---
+++ source/Telemetry.API/Controller/Endpoint.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telemetry.API.Controller
+{
+    public class Endpoint
+    {
+    }
+}
---
+++ source/Telemetry.API/Service/MongoDB.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telemetry.API.Service
+{
+    public class MongoDB
+    {
+        public MongoDB()
+        {
+            MongoClient dbClient = new MongoClient();
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
